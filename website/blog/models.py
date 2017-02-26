3fe6b2c2f40c2603b489f701bc5b832b02ff7b73
Victor Alessander <alessander@protonmail.com>
2017-02-25 23:02:25 -0300

    Added class Cadastro

--- website/blog/models.py
+++ website/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Cadastro(models.Model):
+
+	name = models.CharField(max_length=100)
+	email = models.EmailField(max_length=100)
+	username = models.CharField(max_length=40)
+	password = models.CharField(max_length=50)
