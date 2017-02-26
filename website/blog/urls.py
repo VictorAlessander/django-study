7513c4f17cd56bcd066f3fb0a76d7c3e08dd8e56
Victor <alessander@protonmail.com>
2017-02-26 19:37:30 -0300

    Integration between pages: cadastro and entrar

--- website/blog/urls.py
+++ website/blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^cadastro/', views.cadastro, name='cadastrar'),
+	url(r'^cadastro/', views.cadastro, name='cadastro'),
 	url(r'^entrar/', views.entrar, name='entrar'),
 ]
