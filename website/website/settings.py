2a69713d7da9d31b00bd39950c701821759ba753
Victor Alessander <alessander@protonmail.com>
2017-02-27 01:42:21 -0300

    Added LOGIN_REDIRECT_URL and changed database sqlite3 to PostgresSQL

--- website/website/settings.py
+++ website/website/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_study',
+        'USER': 'vagr',
+        'PASSWORD': '1003victor',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -131,3 +135,5 @@ STATICFILES_DIR = [
 TEMPLATES_DIRS = [
     os.path.join(BASE_DIR, 'templates')
 ]
+
+LOGIN_REDIRECT_URL = '/'
