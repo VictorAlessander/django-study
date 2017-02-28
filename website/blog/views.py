1e03f3df7be80901f68bff71391ee59c08f3d17b
Victor Alessander <alessander@protonmail.com>
2017-02-28 18:12:05 -0300

    Added 'novo_post' function and little change in 'index' function for show the existents posts

--- website/blog/views.py
+++ website/blog/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
-#from .forms import CadastroForm
+from .forms import PostForm
+from .models import Post
 from django.contrib.auth.forms import UserCreationForm
+from django.utils import timezone
 
 # Create your views here.
 
 
 def index(request):
-
-	return render(request, 'index.html')
+	lista_posts = Post.objects.all().order_by('-id')
+	return render(request, 'index.html', {'lista_posts': lista_posts})
 
 
 def cadastro(request):
@@ -28,3 +30,21 @@ def cadastro(request):
 def entrar(request):
 
 	return render(request, 'entrar.html', {})
+
+
+def novo_post(request):
+
+	if request.method == 'POST':
+		form_post = PostForm(request.POST)
+
+		if form_post.is_valid():
+			post = form_post.save(commit=False)
+			post.author = request.user
+			post.date_created = timezone.now()
+			post.save()
+			return redirect('/')
+
+	else:
+		form_post = PostForm()
+
+	return render(request, 'novo_post.html', {'form_post': form_post})
