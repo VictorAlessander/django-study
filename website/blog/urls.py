cb6615404352d4304e5a9a0815df18ee881d9107
Victor Alessander <alessander@protonmail.com>
2017-02-28 18:07:01 -0300

    Removed old comments and added url 'novo_post/'

--- website/blog/urls.py
+++ website/blog/urls.py
@@ -8,11 +8,8 @@ from django.contrib.auth.views import logout
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-#	url(r'^cadastro/', CreateView.as_view(
-#						template_name='cadastro.html',
-#						form_class=UserCreationForm,
-#						success_url='/'), name='cadastro'),
 	url(r'^cadastro/', views.cadastro, name='cadastro'),
 	url(r'^entrar/', login, name='entrar', kwargs={'template_name': 'entrar.html'}),
 	url(r'^sair/', logout, name='sair', kwargs={'template_name': 'index.html'}),
+	url(r'^novo_post/', views.novo_post, name='novo_post'),
 ]
