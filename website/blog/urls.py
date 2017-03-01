3709e3836f882f477a60e52f6c659ccaaad85da0
Victor Alessander <alessander@protonmail.com>
2017-02-28 21:40:54 -0300

    Fixed redirection after logout

--- website/blog/urls.py
+++ website/blog/urls.py
@@ -2,14 +2,12 @@ from django.conf.urls import url
 from . import views
 from django.contrib.auth.views import login
 from django.contrib.auth.views import logout
-#from django.views.generic.edit import CreateView
-#from django.contrib.auth.forms import UserCreationForm
 
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^cadastro/', views.cadastro, name='cadastro'),
 	url(r'^entrar/', login, name='entrar', kwargs={'template_name': 'entrar.html'}),
-	url(r'^sair/', logout, name='sair', kwargs={'template_name': 'index.html'}),
+	url(r'^sair/', logout, name='sair', kwargs={'next_page': '/'}),
 	url(r'^novo_post/', views.novo_post, name='novo_post'),
 ]
