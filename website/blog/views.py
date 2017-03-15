b867627629fd5a041cd80916e5f0534cf784205b
Victor Alessander <alessander@protonmail.com>
2017-03-14 23:13:35 -0300

    Added editar_post and remover_post functions and some little changes

--- website/blog/views.py
+++ website/blog/views.py
@@ -4,6 +4,8 @@ from .models import Post
 from django.contrib.auth.forms import UserCreationForm
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+#from django.http import JsonResponse  # Exemplo de retorno em Json
 
 # Create your views here.
 
@@ -27,7 +29,7 @@ def cadastro(request):
 	return render(request, 'cadastro.html', {'form': form})
 
 
-@login_required()
+@login_required
 def novo_post(request):
 
 	if request.method == 'POST':
@@ -49,3 +51,42 @@ def novo_post(request):
 def visualizar_post(request, id):
 	post_id = Post.objects.filter(id=id)
 	return render(request, 'visualizar_post.html', {'posts': post_id})
+
+
+@login_required
+def editar_post(request, id):
+	post_id = Post.objects.filter(id=id)
+	post = get_object_or_404(Post, id=post_id)
+
+	if request.method == 'POST':
+		form_post = PostForm(request.POST, instance=post)
+
+		if form_post.is_valid():
+			post = form_post.save(commit=False)
+			post.author = request.user
+			post.date_created = timezone.now()
+			post.save()
+			return redirect('/', id=post_id)
+	else:
+		form_post = PostForm(instance=post)
+
+	return render(request, 'editar_post.html', {'form_post': form_post})
+
+
+@login_required
+def remover_post(request, id):
+	post = get_object_or_404(Post, id=id)
+	post.delete()
+
+	return redirect('/')
+
+# Exemplo de retorno em Json
+
+#def api(request):
+#	response_data = {
+#		"id": 4,
+#		"name": 'Testing Response',
+#		"roles": ['Admin', 'User']
+#	}
+
+#	return JsonResponse(response_data)
